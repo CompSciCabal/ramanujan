3dbb34eed72a826d62f61d0ab84862107fcf111a
dann <gh@mrmuster.com>
2024-09-12 20:37:20 -0400

    Added disinterestingness, fixed off-by-one bug
    Co-authored-by: John Krasnay <jkrasnay@users.noreply.github.com>

--- daygen.js
+++ daygen.js
@@ -1,5 +1,6 @@
 const limit = 120 * 365
 const out = []
+let by_day = {}
 
 // some interesting numbers
 
@@ -109,15 +110,15 @@ function squares() {
 
 // listed in order of "interestingness"
 
-let streams = [ pow10
-              , pow2
-              , fac
-              , fib
-              , squares
-              , tri
+let streams = [ pow10 // log(n) abundance
+              , pow2  // log(n) abundance
+              , fac   // much smaller than log(n) abundance
+              , fib   // log(n) abundance
+              , squares // n^()1/2 abundance
+              , tri     // n^()1/2 abundance
               ]
 
-// TIMING!
+//TIMING!
 
 let seconds = {
   name:    "seconds",
@@ -168,30 +169,79 @@ let years = {
   streams: streams.map(s => s())
 }
 
-let factors = [seconds, minutes, hours, days, weeks, months, years]
+let factors = [years, months, weeks, days, hours, minutes, seconds]
 
 for (let day = 0; day < limit; day++) {
-  factors.forEach(f => {
+  factors.forEach((f, fi) => {
     if(f.start(day) === f.end(day))
       return
-    f.streams.forEach(s => {
-      while (f.start(day) <= s.cur && s.cur < f.end(day)) {
+    f.streams.forEach((s, si) => {
+      while (f.start(day) < s.cur && s.cur <= f.end(day)) {
         // out.push({ day, name: `${s.name} ${f.name}`, value: s.cur })
-        out.push({ day, type: s.name, time: f.name, value: s.cur })
+        out.push({ day, type: s.name, time: f.name, value: s.cur, dis: dinterestingness(fi, si) })
         s.next()
       }
     })
   })
 }
 
+by_day = out.reduce((acc, e) => {acc[e.day] = (acc[e.day]||[]).concat(e); return acc}, {})
+
 console.log(out)
 
 let el = document.getElementById.bind(document)
 el('out').innerHTML = out
 
+let interesting_limit = 8
+let upcoming_limit = 100
+
+el('bday').addEventListener('change', e => {
+  // console.log(e)
+  let ts = e.target.valueAsNumber
+  let t  = (new Date()).getTime()
+  let day = Math.floor((t - ts) / 1000 / 60 / 60 / 24)
+  let today = by_day[day]
+  el('today').innerHTML = JSON.stringify(today, 2, 2)
+  // anything interesting coming up?
+  upcoming()
+})
+
+function upcoming() {
+  let ts = el('bday').valueAsNumber
+  let t  = (new Date()).getTime()
+  let day = Math.floor((t - ts) / 1000 / 60 / 60 / 24)
+  let today = by_day[day]
+  let interesting_things = []
+  for(let i = 0; i<upcoming_limit; i++) {
+    interesting_things = interesting_things.concat(by_day[day+i].filter(e => e.dis <= interesting_limit))
+  }
+  interesting_things = interesting_things.map(e => {
+    e.date = new Date(ts + e.day*24*60*60*1000)
+    return e
+  })
+  el('upcoming').innerHTML = JSON.stringify(interesting_things.sort((a, b) => a.dis-b.dis), 2, 2)
+}
+
+el('intlimit').addEventListener('change', e => {
+  interesting_limit = e.target.value
+  upcoming()
+})
+
+el('uplimit').addEventListener('change', e => {
+  upcoming_limit = e.target.value
+  upcoming()
+})
+
+// TODO: increase interestingness of e.g. square of 50
+function dinterestingness(factor, stream) {
+  return (factor+1) * (stream+1)
+}
+
+
 let unique = xs => xs.reduce((acc, x, i) => xs.indexOf(x) === i ? acc.concat(x) : acc, [])
 let range = n => [...Array(n).keys()]
 let missing = (all, some) => all.filter(x => !some.includes(x))
+
 
 // slow!
 // let all_days = range(limit)
@@ -207,16 +257,17 @@ console.log(bad_days)
 
 
 // TODO:
+// taxicab numbers!
 // maybe pow10 in dog years, cat years, etc? I'm not sure how to do that...
 // can also examine other units of time, like time it takes light to get to the nearest stars,
-// years on other planets (I'm ten years old on Saturn today!, I'm 1000 Neptunian days old!),
-// etc <-- maybe these need a different loop, just for one factor...
+// years on other planets (I'm ten years old on Saturn today!, I'm 1000  ()slightly less abundant than loNeptunian days old!),
+//g(n)/ etc <-- maybe these) need a different loop, just for one factor...
 
 // OEIS sequences:
-// https://oeis.org/A000396 Perfect numbers
+// https://oei (n^(1/3) abundance)s.org/ (abundance somewhere between log(n) and the abundance of perfect numberA000396 Perfect numbers
 // https://oeis.org/A000108 Catalan numbers
 // https://oeis.org/A000668 Mersenne primes
-// https://oeis.org/A000578 The cubes
+// https: (log(n) abun)ance//oeis.org/A000578 The cubes
 // https://oeis.org/A034897 Hyperperfect numbers
 // https://oeis.org/A019279 Superperfect numbers
 // https://oeis.org/A006862 Euclid numbers
